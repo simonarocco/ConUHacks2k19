e7718a9a8df3e27436a978771cbdc3935e545727
simonarocco <37022160+simonarocco@users.noreply.github.com>
2019-01-26 18:04:52 -0500

    setting up node js server correctly

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ var path = require('path');
 
 var app = express();
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 7000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 app.locals.pretty = true;
+app.get("/", (req, res) => {
+    res.redirect("/BuildProfile1.html");
+});
 
 http.createServer(app).listen(app.get('port'), function() {
  console.log('Express server listening on port ' + app.get('port'));
