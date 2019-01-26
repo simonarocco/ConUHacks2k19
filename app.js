808e1429c1d4d9da74e1c893bf06f2fdfb294fa5
simonarocco <37022160+simonarocco@users.noreply.github.com>
2019-01-26 17:49:57 -0500

    deleted irrelevant files

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
